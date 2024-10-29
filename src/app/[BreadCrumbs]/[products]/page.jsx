92c233c4340603dfe5fc23defa4e154de80d5c0c
Pradeep-Prady <pradeepprady005@gmail.com>
2024-10-29 15:03:31 +0530

    host 12

---
+++ src/app/[BreadCrumbs]/[products]/page.jsx
@@ -0,0 +1,35 @@
+import ClientProvider from '@/components/utils/ClientProvider';
+ 
+import ProductMap from './_ProductMap';
+ 
+import { bkend } from '@/axios/axiosInstance';
+
+export default async function ProductPage() {
+
+	const response = await bkend.get('/getproduct'); 
+	console.log("🚀 ~ ProductPage ~ response:", response.data);
+
+	const prod = [{
+		name: 'Buscuit TCups',
+		cost: 50,
+		suffix: '(60 ml)'
+	}, {
+		name: 'Buscuit TCups',
+		cost: 60,
+	}, {
+		name: 'Buscuit TCups',
+		cost: 20,
+		suffix: '(60 ml)'
+	}];
+
+	if (response) {
+		return (
+			<main className='bg-baseBg evenPadding'>
+				<ClientProvider>
+					<ProductMap prod={response.data} />
+				</ClientProvider>
+			</main>
+		)
+	}
+}
+
--- src/app/[BreadCrumbs]/layout.jsx
+++ src/app/[BreadCrumbs]/layout.jsx
@@ -1,15 +1,15 @@
-// import Footer from "@/components/common/Footer";
-// // import Header from "@/components/common/Header";
-// // import ReduxndQueryWrapper from "@/components/home/ReduxndQueryWrapper";
+import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
+import ReduxndQueryWrapper from "@/components/home/ReduxndQueryWrapper";
 
-// export default function layout({ children }) {
-//   return (
-//     <>
-//       {/* <ReduxndQueryWrapper>
-//         <Header />
-//       </ReduxndQueryWrapper> */}
-//       {children}
-//       <Footer />
-//     </>
-//   );
-// }
+export default function layout({ children }) {
+  return (
+    <>
+      <ReduxndQueryWrapper>
+        <Header />
+      </ReduxndQueryWrapper>
+      {children}
+      <Footer />
+    </>
+  );
+}
