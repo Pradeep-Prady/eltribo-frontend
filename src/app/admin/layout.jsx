6380cb52463754538348c4ecda86f3682922e1f5
Pradeep-Prady <pradeepprady005@gmail.com>
2024-10-29 16:20:44 +0530

    host 27

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -1,24 +1,18 @@
-'use client';
+"use client";
 
-import ClientProvider from '@/components/utils/ClientProvider';
-import { QueryClient, QueryClientProvider } from 'react-query';
-
+import ClientProvider from "@/components/utils/ClientProvider";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function Layout({ children }) {
-	
-	const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
-	return (
-		// <QueryClientProvider client={queryClient}>
-		// 	<ClientProvider>
-		// 		<div>{children}</div>
-		// 	</ClientProvider>
-		// </QueryClientProvider>
-
-		<>
-
-		</>
-	)
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <ClientProvider>
+          <div>{children}</div>
+        </ClientProvider>
+      </QueryClientProvider>
+    </>
+  );
 }
-
-
