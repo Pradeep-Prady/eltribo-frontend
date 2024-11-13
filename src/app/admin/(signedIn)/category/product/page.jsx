167f86abad66a1650752b72356ae102cb0cbfef7
Pradeep-Prady <pradeepprady005@gmail.com>
2024-11-13 16:38:52 +0530

    ui changes 23

--- src/app/admin/(signedIn)/category/product/page.jsx
+++ src/app/admin/(signedIn)/category/product/page.jsx
@@ -1,9 +1,9 @@
 
-// import React from 'react'
+import React from 'react'
 
-// export default function Product() {
-// 	return (
-// 		<div>Product</div>
-// 	)
-// }
-// // €
+export default function Product() {
+	return (
+		<div>Product</div>
+	)
+}
+// €
