8b0c7dfd0d6c76aa1db721c84018e8fd6c26056f
Pradeep-Prady <pradeepprady005@gmail.com>
2024-11-13 15:43:45 +0530

    ui changes 22

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -4,14 +4,14 @@ const localUrl = "http://localhost:5000/api/";
 const prodUrl = "https://eltribo-server.vercel.app/api/";
 
 export const bkend = axios.create({
-  baseURL: localUrl,
+  baseURL: prodUrl,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export const db = axios.create({
-  baseURL: localUrl,
+  baseURL: prodUrl,
   headers: {
     "Content-Type": "application/json",
   },
